ed54b1b683889ef16a9b88e7e423b88518aae524
JustPause <Justinaslolt@gmail.com>
2025-11-09 17:42:11 +0200

    Hmmmm

--- src/KnygosSuradimasPabalISBN.py
+++ src/KnygosSuradimasPabalISBN.py
@@ -9,4 +9,4 @@ def scanner(file):
             isbn = input()
             for index, row in enumerate(rows):  
                 if (row.get("isbn") == isbn or row.get("Kodas") == isbn):
-                    print(row["Pavadinimas"] + " " +row.get("Komentarai"))
+                    print(row["Pavadinimas"] + " " + row.get("Komentarai", ""))
