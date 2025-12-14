3ea02dcc9eed9d2d3f3c70c7342ca31528d3f829
Justinas <iamjuststan@hotmail.com>
2025-12-14 04:08:06 +0200

    Good

--- src/quickstart.py
+++ src/quickstart.py
@@ -9,25 +9,21 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
+sheet = None
+sheet_id= None
+sheet_range= None
+
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
 
+    global sheet,sheet_id,sheet_range
+
     token = "src/.env/token.json"
     client_secret = "src/.env/client_secret.json"
     creds = None
     sheet = None
-    sheet_id = None
-    rage = None
-
-    with open("src/.env/sheet.json", 'r') as sheet_json:
-        sheet = json.load(sheet_json)
-
-        # sheet = json.loads(sheet_json)
-        print(sheet["sheet_id"])
-        sheet_id = sheet["sheet_id"]
-        sheet_range = sheet["rage"]
 
     if os.path.exists(token):
         creds = Credentials.from_authorized_user_file(token, SCOPES)
@@ -41,27 +37,38 @@ def main():
         with open(token, "w") as token:
             token.write(creds.to_json())
 
+    with open("src/.env/sheet.json", 'r') as sheet_json:
+        sheet = json.load(sheet_json)
+
+        sheet_id = sheet["sheet_id"]
+        sheet_range = sheet["range"]
+        
     try:
         service = build("sheets", "v4", credentials=creds)
 
         sheet = service.spreadsheets()
-        result = (
+        
+        # print("Name, Major:")
+        # for row in values:
+        #     print(row)
+    except HttpError as err:
+        print(err)
+
+def getData(sheet_range):
+    global sheet,sheet_id
+    result = (
             sheet.values()
             .get(spreadsheetId=sheet_id, range=sheet_range)
             .execute()
         )
-        values = result.get("values", [])
+    values = result.get("values", [])
 
-        if not values:
-            print("No data found.")
-            return
-
-        print("Name, Major:")
-        for row in values:
-            print(row)
-    except HttpError as err:
-        print(err)
-
+    if not values:
+        print("No data found.")
+        return None
+    
+    return values
 
 if __name__ == "__main__":
     main()
+    print(getData("VIsos knygos!B2:B"))
