fc3576d630730551b53cbc8e9e182d2252fc4cf2
Justinas <iamjuststan@hotmail.com>
2025-12-20 13:34:36 +0200

    HM

--- src/gui/main.py
+++ src/gui/main.py
@@ -24,25 +24,23 @@ class Pagrindinis ( wx.Frame ):
 
         self.SetSizeHints( wx.Size( 1280,720 ), wx.Size( 1280,720 ) )
 
-        fgSizer2 = wx.FlexGridSizer( 0, 2, 0, 0 )
-        fgSizer2.SetFlexibleDirection( wx.BOTH )
-        fgSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_ALL )
+        bSizer2 = wx.BoxSizer( wx.VERTICAL )
 
         self.m_panel1 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 256,700 ), wx.BORDER_NONE )
         self.m_panel1.SetBackgroundColour( wx.Colour( 201, 201, 201 ) )
 
-        fgSizer2.Add( self.m_panel1, 0, wx.EXPAND, 0 )
+        bSizer2.Add( self.m_panel1, 0, wx.EXPAND, 0 )
 
         self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 1000,700 ), wx.BORDER_NONE )
         self.m_panel2.SetBackgroundColour( wx.Colour( 217, 217, 217 ) )
 
-        fgSizer2.Add( self.m_panel2, 1, wx.EXPAND, 0 )
+        bSizer2.Add( self.m_panel2, 1, wx.EXPAND, 0 )
 
 
-        self.SetSizer( fgSizer2 )
+        self.SetSizer( bSizer2 )
         self.Layout()
 
-        self.Centre( wx.BOTH )
+        self.Centre( wx.HORIZONTAL )
 
     def __del__( self ):
         pass
