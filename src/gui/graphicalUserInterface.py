a26028f52fda87be3b5583ccbde90c97bd8a6411
Justinas <iamjuststan@hotmail.com>
2025-12-28 19:38:41 +0200

    Update

--- src/gui/graphicalUserInterface.py
+++ src/gui/graphicalUserInterface.py
@@ -8,13 +8,19 @@ class GUI(wx.Frame):
     def __init__(self, parent):    
         wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = gettext.gettext(u"Pagrindinis"), pos = wx.DefaultPosition, size = wx.Size( 1280,720 ), style = wx.CLOSE_BOX|wx.DEFAULT_FRAME_STYLE )
         
-        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        
         self.sideBar = SideBar(self)
         self.mainPanel = Pagrindinis(self)
-        mainSizer.Add(self.sideBar, 0, wx.EXPAND, 0)
-        mainSizer.Add(self.mainPanel, 1, wx.EXPAND, 0)
-        self.SetSizer(mainSizer)
+        
+        self.mainSizer.Add(self.sideBar, 0, wx.EXPAND, 0)
+        self.mainSizer.Add(self.mainPanel, 1, wx.EXPAND, 0)
+        
+        self.SetSizer(self.mainSizer)
         self.Layout()
+
+    def ReplacePanel(self):
+        self.mainSizer.Detach(self.mainPanel)
 
 if __name__ == "__main__":
     app = wx.App(False)
--- src/gui/overrideMain.py
+++ src/gui/overrideMain.py
@@ -1,13 +1,19 @@
+import wx
 import wxformbuilder
 
 class SideBar(wxformbuilder.SideBar): 
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+
     def Click(self, event):
         print("Button was clicked!")
-        # Do your logic here
-
-        # Call this only if you want other handlers to run too
+        
         event.Skip()
         
 
 class Pagrindinis(wxformbuilder.Pagrindinis): 
+    pass
+
+class ISNBkoduAtspauzdinimas(wxformbuilder.ISNBkoduAtspauzdinimas):
     pass
