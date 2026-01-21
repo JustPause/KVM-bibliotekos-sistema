6010480c09431b2af3663635a4fde6d1ccadbd3a
JustPause <Justinaslolt@gmail.com>
2026-01-21 18:02:00 +0200

    update

--- Programuotojo_Instrucija.md
+++ Programuotojo_Instrucija.md
@@ -1,6 +1,6 @@
 # Programa Barkodas
 
-###### versija: 0.1.0 
+###### Versija: 0.1.0 
 ###### Data: 2026 01 21
 	
 Programa leidžia lengviau atlikti darbus bibliotekos **Google Sheets** lentelėse. Ji sukurta specifiškai pagal **Kauno Valdorfo mokyklos** reikalavimus, todėl gali nebūti idealiai pritaikyta kitoms bibliotekų sistemoms. Tačiau, suprantant **kodą**, galima nesunkiai pridėti bet kokį norimą funkcionalumą.
--- mdToPDF.py
+++ mdToPDF.py
@@ -4,14 +4,13 @@ from markdown import markdown
 from weasyprint import CSS, HTML
 
 md_README_path = Path("README.md")
-md_naudojimo_Instrucija_path = Path("Naudojimo_Instrucija.md")
+md_naudojimo_Instrucija_path = Path("Naudotojo_Instrucija.md")
 md_programuotojo_Instrucija_path = Path("Programuotojo_Instrucija.md")
 pdf_path = Path("README.pdf")
 css_path = Path("src/css/mdfile.css")
 
 
 def makeMDtoPDF(md_path: Path, css_path: Path):
-    print()
     md_text = md_path.read_text(encoding="utf-8")
 
     html_body = markdown(
@@ -28,13 +27,21 @@ def makeMDtoPDF(md_path: Path, css_path: Path):
     </body>
     </html>
     """
-
-    HTML(string=html, base_url=md_path.parent).write_pdf(
-        Path("docs") / md_path.with_suffix(".pdf").name,
-        stylesheets=[CSS(filename=css_path)],
+    dirName="docs"
+    Path(dirName).mkdir(parents=True, exist_ok=True)
+    output_pdf = (Path("docs") / md_path.with_suffix(".pdf").name).resolve()
+    
+    HTML(
+        string=html,
+        base_url=md_path.parent.resolve(),
+    ).write_pdf(
+        target=output_pdf,
+        stylesheets=[CSS(filename=css_path.resolve())],
     )
+    
+    print("Saved:", output_pdf)
 
 
 makeMDtoPDF(md_README_path, css_path)
 makeMDtoPDF(md_naudojimo_Instrucija_path, css_path)
-# makeMDtoPDF(md_programuotojo_Instrucija_path, css_path)
+makeMDtoPDF(md_programuotojo_Instrucija_path, css_path)
